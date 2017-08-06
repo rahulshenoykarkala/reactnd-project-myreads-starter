03ddd4e279a1bc571adf13260bd9f057fd745cd2
Rahul Shenoy <Rahul_Shenoy@amat.com>
2017-08-06 10:51:20 +0530

    Created Book component for better  modularization

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Book from './Book.js'
 
 class Bookshelf extends Component {
     render(){
@@ -10,28 +11,7 @@ class Bookshelf extends Component {
                     {
                         this.props.bookList.map((book) => (
                             <li key={book.id}>
-                                <div className="book">
-                                    <div className="book-top">
-                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                                    <div className="book-shelf-changer">
-                                        <select value={book.shelf} id={book.id} onChange={(e) => {
-                                                this.props.updateBookStatus(e.target)
-                                            }}>
-                                        <option value="none" disabled>Move to...</option>
-                                        <option value="currentlyReading">Currently Reading</option>
-                                        <option value="wantToRead">Want to Read</option>
-                                        <option value="read">Read</option>
-                                        <option value="none">None</option>
-                                        </select>
-                                    </div>
-                                    </div>
-                                    <div className="book-title">{book.title}</div>
-                                    <div className="book-authors">
-                                        {
-                                            book.authors.map((author, index)=>(<div key={index}>{author}</div>))
-                                        }
-                                    </div>
-                                </div>
+                                <Book book={book}/>
                             </li>
                         ))
                     }
